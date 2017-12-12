d1351a702c0a9ddc7baf8a3e80b035548bbfb2c3
austindeh <31968625+austindeh@users.noreply.github.com>
2017-12-11 21:34:33 -0800

    I broke it.

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,65 @@
 var express = require('express');
 var router = express.Router();
 
+const projects = [
+  {
+    path: 'alpinestars-adaptive-stretch',
+    title: 'Alpinestars Adaptive Stretch',
+    body: 'For 2017 Alpinestars was launching a new gear set which implemented the use of new innovative stretch fabrics which promoted comfortable adaptive flexibility for the rider. I was tasked to come up with a concept and execution to show the functionality of the innovative new fabrics. The fabrics were in select key areas which helped increase rider comfort in certain positions while on the bike. While stretch fabrics are common among other brands, Alpinestars reinvented the way the gear worked with the rider. Our goal was to show this through simple and intriguing imagery.',
+    client: 'Alpinestars',
+    date: '2016',
+    role: 'Art Direction, Photography, Design'
+  },
+  {
+    path: 'made-in-la',
+    title: 'Made In LA',
+    body: 'Made in LA is a new Los Angeles based music group. Our mission for the Made in LA rebrand was very simple. We wanted to not only mark the beginning of a new artist but also to tell their story through a timeless icon. My goal was to create something which would represent their music, style and personality in a visually pleasing way. Made in LA is minimal, independant, defined and more importantly, new.',
+    client: 'Made In LA',
+    date: '2016',
+    role: 'Art Direction, Design'
+  },
+  {
+    path: 'motoamerica',
+    title: 'Motoamerica',
+    body: 'It was an honor to be able to influence a sport I love so much through my art and design. Coming from a strong racing heritage and successful racing career it seemed to be a perfect fit for me to help with the start of the new Pro American Motorcycle Championship. I was given a lot of freedom in this project creatively and had the opportunity to really push my capabilities as a designer. This project was completed over the course of nearly a year and involved everything from logo development, brand identity, sales & media decks, at track collateral, business collateral, print advertising, web advertising, websites, and supporting logos.',
+    client: 'Motoamerica',
+    date: '2015',
+    role: 'Art Direction, Branding, Design'
+  },
+  {
+    path: 'ken-block',
+    title: 'Ken Block x Hoonigan',
+    body: 'Made in LA is a new Los Angeles based music group. Our mission for the Made in LA rebrand was very simple. We wanted to not only mark the beginning of a new artist but also to tell their story through a timeless icon. My goal was to create something which would represent their music, style and personality in a visually pleasing way. Made in LA is minimal, independant, defined and more importantly, new.',
+    client: 'Hoonigan',
+    date: '2014',
+    role: 'Illustration, Design'
+  },
+  {
+    path: 'archives',
+    title: 'Archives',
+    body: '',
+    client: '',
+    date: '',
+    role: ''
+  }
 
-router.get('/alpinestars-adaptive-stretch', function(req, res, next) {
+]
+
+for (var i = 0; i < projects.length; i++) {
+  var project = project[i]
+  var nextLink, prevLink
+
+  if (i === 0) {
+    prevLink = projects.length - 1
+  } else {
+    prevLink = i - i
+  }
+  router.get('/' + project.path, function(req, res, next) {
+    res.render('project', { project: project, images: [] })
+  });
+}
+
+/* router.get('/alpinestars-adaptive-stretch', function(req, res, next) {
   var project = {
     title: 'Alpinestars Adaptive Stretch',
     body: 'For 2017 Alpinestars was launching a new gear set which implemented the use of new innovative stretch fabrics which promoted comfortable adaptive flexibility for the rider. I was tasked to come up with a concept and execution to show the functionality of the innovative new fabrics. The fabrics were in select key areas which helped increase rider comfort in certain positions while on the bike. While stretch fabrics are common among other brands, Alpinestars reinvented the way the gear worked with the rider. Our goal was to show this through simple and intriguing imagery.',
@@ -55,7 +112,7 @@ router.get('/archives', function(req, res, next) {
     role: ''
   }
   res.render('project', { project: project })
-});
+}); */
 
 
 module.exports = router;
