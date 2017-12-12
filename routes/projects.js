fd352b393c5a662d08e2810388f4f87d65ed1585
Troy Curnel <NearSingularity@troycurnel.com>
2017-12-12 01:00:13 -0800

    Revert to declarative routes

--- routes/projects.js
+++ routes/projects.js
@@ -1,87 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var projects = [
-  {
-    path: 'alpinestars-adaptive-stretch',
-    title: 'Alpinestars Adaptive Stretch',
-    body: 'For 2017 Alpinestars was launching a new gear set which implemented the use of new innovative stretch fabrics which promoted comfortable adaptive flexibility for the rider. I was tasked to come up with a concept and execution to show the functionality of the innovative new fabrics. The fabrics were in select key areas which helped increase rider comfort in certain positions while on the bike. While stretch fabrics are common among other brands, Alpinestars reinvented the way the gear worked with the rider. Our goal was to show this through simple and intriguing imagery.',
-    client: 'Alpinestars',
-    date: '2016',
-    role: 'Art Direction, Photography, Design'
-  },
-  {
-    path: 'made-in-la',
-    title: 'Made In LA',
-    body: 'Made in LA is a new Los Angeles based music group. Our mission for the Made in LA rebrand was very simple. We wanted to not only mark the beginning of a new artist but also to tell their story through a timeless icon. My goal was to create something which would represent their music, style and personality in a visually pleasing way. Made in LA is minimal, independant, defined and more importantly, new.',
-    client: 'Made In LA',
-    date: '2016',
-    role: 'Art Direction, Design'
-  },
-  {
-    path: 'motoamerica',
-    title: 'Motoamerica',
-    body: 'It was an honor to be able to influence a sport I love so much through my art and design. Coming from a strong racing heritage and successful racing career it seemed to be a perfect fit for me to help with the start of the new Pro American Motorcycle Championship. I was given a lot of freedom in this project creatively and had the opportunity to really push my capabilities as a designer. This project was completed over the course of nearly a year and involved everything from logo development, brand identity, sales & media decks, at track collateral, business collateral, print advertising, web advertising, websites, and supporting logos.',
-    client: 'Motoamerica',
-    date: '2015',
-    role: 'Art Direction, Branding, Design'
-  },
-  {
-    path: 'ken-block',
-    title: 'Ken Block x Hoonigan',
-    body: 'Made in LA is a new Los Angeles based music group. Our mission for the Made in LA rebrand was very simple. We wanted to not only mark the beginning of a new artist but also to tell their story through a timeless icon. My goal was to create something which would represent their music, style and personality in a visually pleasing way. Made in LA is minimal, independant, defined and more importantly, new.',
-    client: 'Hoonigan',
-    date: '2014',
-    role: 'Illustration, Design'
-  },
-  {
-    path: 'archives',
-    title: 'Archives',
-    body: '',
-    client: '',
-    date: '',
-    role: ''
-  }
 
-]
-
-for (var i = 1; i < projects.length; i++) {
-  var j = i - 1;
-  var project = projects[j];
-  var nextProject = projects[i];
-
-  var p = Object.assign({
-    prevProject: j === 0 ? projects[projects.length - 1].path : projects[j - 1].path,
-    nextProject: nextProject.path
-  }, project)
-
-  router.get('/' + p.path, function(req, res, next) {
-    res.render('project', { project: p, images: [] })
-  });
-
-  console.log(p)
-
-  if (i === projects.length - 1) {
-    var pp = Object.assign({
-      prevProject: projects[j - 1].path,
-      nextProject: projects[0].path
-    }, nextProject)
-
-    router.get('/' + pp.path, function(req, res, next) {
-      res.render('project', { project: pp, images: [] })
-    });
-
-    console.log(pp)
-  }
-}
-
-/* router.get('/alpinestars-adaptive-stretch', function(req, res, next) {
+router.get('/alpinestars-adaptive-stretch', function(req, res, next) {
   var project = {
     title: 'Alpinestars Adaptive Stretch',
     body: 'For 2017 Alpinestars was launching a new gear set which implemented the use of new innovative stretch fabrics which promoted comfortable adaptive flexibility for the rider. I was tasked to come up with a concept and execution to show the functionality of the innovative new fabrics. The fabrics were in select key areas which helped increase rider comfort in certain positions while on the bike. While stretch fabrics are common among other brands, Alpinestars reinvented the way the gear worked with the rider. Our goal was to show this through simple and intriguing imagery.',
     client: 'Alpinestars',
     date: '2016',
-    role: 'Art Direction, Photography, Design'
+    role: 'Art Direction, Photography, Design',
+    prevProject: 'archives',
+    nextProject: 'made-in-la',
   }
   res.render('project', { project: project, images: [] })
 });
@@ -92,7 +21,9 @@ router.get('/made-in-la', function(req, res, next) {
     body: 'Made in LA is a new Los Angeles based music group. Our mission for the Made in LA rebrand was very simple. We wanted to not only mark the beginning of a new artist but also to tell their story through a timeless icon. My goal was to create something which would represent their music, style and personality in a visually pleasing way. Made in LA is minimal, independant, defined and more importantly, new.',
     client: 'Made In LA',
     date: '2016',
-    role: 'Art Direction, Design'
+    role: 'Art Direction, Design',
+    prevProject: 'alpinestars-adaptive-stretch',
+    nextProject: 'motoamerica',
   }
   res.render('project', { project: project })
 });
@@ -103,7 +34,9 @@ router.get('/motoamerica', function(req, res, next) {
     body: 'It was an honor to be able to influence a sport I love so much through my art and design. Coming from a strong racing heritage and successful racing career it seemed to be a perfect fit for me to help with the start of the new Pro American Motorcycle Championship. I was given a lot of freedom in this project creatively and had the opportunity to really push my capabilities as a designer. This project was completed over the course of nearly a year and involved everything from logo development, brand identity, sales & media decks, at track collateral, business collateral, print advertising, web advertising, websites, and supporting logos.',
     client: 'Motoamerica',
     date: '2015',
-    role: 'Art Direction, Branding, Design'
+    role: 'Art Direction, Branding, Design',
+    prevProject: 'made-in-la',
+    nextProject: 'ken-block',
   }
   res.render('project', { project: project })
 });
@@ -114,7 +47,9 @@ router.get('/ken-block', function(req, res, next) {
     body: 'Made in LA is a new Los Angeles based music group. Our mission for the Made in LA rebrand was very simple. We wanted to not only mark the beginning of a new artist but also to tell their story through a timeless icon. My goal was to create something which would represent their music, style and personality in a visually pleasing way. Made in LA is minimal, independant, defined and more importantly, new.',
     client: 'Hoonigan',
     date: '2014',
-    role: 'Illustration, Design'
+    role: 'Illustration, Design',
+    prevProject: 'motoamerica',
+    nextProject: 'archives',
   }
   res.render('project', { project: project })
 });
@@ -125,10 +60,12 @@ router.get('/archives', function(req, res, next) {
     body: '',
     client: '',
     date: '',
-    role: ''
+    role: '',
+    prevProject: 'ken-block',
+    nextProject: 'alpinestars-adaptive-strech',
   }
   res.render('project', { project: project })
-}); */
+});
 
 
 module.exports = router;
