19e4074687740c09af177120d4529b61774ad3cf
austindeh <31968625+austindeh@users.noreply.github.com>
2018-01-17 16:56:39 -0800

    image filter styling

--- public/javascript/global.js
+++ public/javascript/global.js
@@ -8,11 +8,8 @@ function checkScroll() {
 }
 
 $(function() {
-    // Looks gross.
-    $("body").removeClass("ghost")
 
     checkScroll()
-
     $(window).scroll(checkScroll)
 
     $('#back-to-top').click(function() {
