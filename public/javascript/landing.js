705722c994c957c533bff446c55d316fa8e2e2e0
austindeh <31968625+austindeh@users.noreply.github.com>
2018-02-05 15:46:53 -0800

    scroll down button clean up

--- public/javascript/landing.js
+++ public/javascript/landing.js
@@ -1,7 +1,9 @@
+// Reveal social buttons on name click
 $("#name").click(function() {
   $("#social-button").fadeToggle("fast", "swing")
 })
 
+// Hide scroll down button on scroll
 $(window).scroll(function() {
 	if (window.scrollY > 50) {
 	  $(".scroll-down").css("opacity", 0)
@@ -10,12 +12,14 @@ $(window).scroll(function() {
   }
 })
 
-$(".scroll-down").click(function() {
+// Scroll down to project on scroll down button click
+$("#scroll-down").click(function() {
   $("html, body").animate({
     scrollTop: $( $('.project')).offset().top - 75
   }, 500);
 });
 
+// Glitch skewing on offest
 var t1 = new TimelineMax({repeat: -1,repeatDelay: 5.3})
 t1.to(".textglitch1", 0.1, {skewX:900,ease: Power4.easeInOut}).to(".textglitch1", 0.04, {skewX:0,ease: Power4.easeInOut})
 
@@ -27,20 +31,22 @@ t3.to(".textglitch3", 0.1, {skewX:400,ease: Power4.easeInOut}).to(".textglitch3"
 
 var t4 = new TimelineMax({repeat: -1,repeatDelay: 5})
 t4.to(".snippetglitch", 0.1, {skewX:700,ease: Power4.easeInOut})
-.to(".snippetglitch", 0.04, {skewX:0,ease: Power4.easeInOut})
-.to(".snippetglitch", 0.02, {x:-20})
-.to(".snippetglitch", 0.03, {x:0})
-.add("split", 0)
-.to(".snippetglitch", 0.02, {scaleY:1.1,ease: Power4.easeInOut})
-.to(".snippetglitch", 0.04, {scaleY:1,ease: Power4.easeInOut})
-.to(".snippetglitch", 0.1, {skewX:70,ease: Power4.easeInOut})
-.to(".snippetglitch", 0.04, {skewX:0,ease: Power4.easeInOut})
-.to(".snippetglitch", 0.04, {x:-20})
-.to(".snippetglitch", 0.04, {x:0})
-.add("split", 0)
-.to(".snippetglitch", 0.01, {scaleY:.4,ease: Power4.easeInOut})
-.to(".snippetglitch", 0.0, {scaleY:1,ease: Power4.easeInOut})
+  .to(".snippetglitch", 0.04, {skewX:0,ease: Power4.easeInOut})
+  .to(".snippetglitch", 0.02, {x:-20})
+  .to(".snippetglitch", 0.03, {x:0})
+  .add("split", 0)
+  .to(".snippetglitch", 0.02, {scaleY:1.1,ease: Power4.easeInOut})
+  .to(".snippetglitch", 0.04, {scaleY:1,ease: Power4.easeInOut})
+  .to(".snippetglitch", 0.1, {skewX:70,ease: Power4.easeInOut})
+  .to(".snippetglitch", 0.04, {skewX:0,ease: Power4.easeInOut})
+  .to(".snippetglitch", 0.04, {x:-20})
+  .to(".snippetglitch", 0.04, {x:0})
+  .add("split", 0)
+  .to(".snippetglitch", 0.01, {scaleY:.4,ease: Power4.easeInOut})
+  .to(".snippetglitch", 0.0, {scaleY:1,ease: Power4.easeInOut})
 
+
+// Scroll Down Text Scramble
 class TextScramble {
   constructor(el) {
     this.el = el
