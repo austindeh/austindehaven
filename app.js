bd414f3217ca377ddb28b9577f09ba2fb6fc0bc1
austindeh <31968625+austindeh@users.noreply.github.com>
2018-03-07 11:24:15 -0800

    analytics and favicon

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var https = require("https");
+var favicon = require('serve-favicon')
+var path = require('path')
 
 var index = require('./routes/index');
 var projects = require('./routes/projects');
@@ -13,6 +15,10 @@ var projects = require('./routes/projects');
 var app = express();
 
 var host = process.env.HOST
+
+// Add Favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
