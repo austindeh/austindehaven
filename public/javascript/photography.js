635b6a47f63f95e76fddde1b86e8fdbdd0cbe3ab
austindeh <31968625+austindeh@users.noreply.github.com>
2018-03-20 17:40:14 -0700

    photography image filter preload personal

--- public/javascript/photography.js
+++ public/javascript/photography.js
@@ -1,4 +1,18 @@
+$(function(){
+    filter = $('.filter-selected').attr('gallery-filter');
+
+    $('.masonry-item').each( function() {
+
+        if (filter == $(this).attr('gallery-category')) {
+            $(this).fadeIn();
+        } else {
+            $(this).hide();
+        }
+    });
+  });
+
 $('document').ready( function($) {
+
   $('.filter-btn').on('click', function() {
       $('.filter-btn').removeClass('filter-selected selected').addClass('underlined');
       $(this).removeClass('underlined').addClass('filter-selected selected');
