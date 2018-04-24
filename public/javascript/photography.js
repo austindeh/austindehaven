0e26eb3cabb055a70e2d79316305a12822d136c2
austindeh <31968625+austindeh@users.noreply.github.com>
2018-04-24 01:00:56 -0700

    why isnt this first image moving. diugaelh

--- public/javascript/photography.js
+++ public/javascript/photography.js
@@ -14,12 +14,12 @@ $(function(){
 $('document').ready( function($) {
 
   $('.filter-btn').on('click', function() {
-      $('.filter-btn').removeClass('filter-selected selected').addClass('underlined');
-      $(this).removeClass('underlined').addClass('filter-selected selected');
-      filter = $(this).attr('gallery-filter');
-
       $('.masonry-item').hide().addClass('fade-out-filter');
 
+      $('.filter-btn').removeClass('filter-selected selected').addClass('underlined');
+      $(this).removeClass('underlined').addClass('filter-selected selected');
+
+      filter = $(this).attr('gallery-filter');
       $('.masonry-item').each( function() {
           if (filter == 'all') {
               $(this).show().removeClass('fade-out-filter').addClass('fade-in-filter');
