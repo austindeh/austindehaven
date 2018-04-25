616cffd822b591b90cc216a2b60648d32ae848e7
austindeh <31968625+austindeh@users.noreply.github.com>
2018-04-24 19:35:38 -0700

    idk what im doing

--- public/javascript/photography.js
+++ public/javascript/photography.js
@@ -1,7 +1,7 @@
-$(function(){
+$(function () {
     filter = $('.filter-selected').attr('gallery-filter');
 
-    $('.masonry-item').each( function() {
+    $('.masonry-item').each(function () {
 
         if (filter == $(this).attr('gallery-category')) {
             $(this).show();
@@ -9,34 +9,34 @@ $(function(){
             $(this).hide();
         }
     });
-  });
+});
 
-$('document').ready( function($) {
+$('document').ready(function ($) {
 
-  $('.filter-btn').on('click', function() {
-      $('.masonry-item').hide().addClass('fade-out-filter');
+    $('.filter-btn').on('click', function () {
+        $('.masonry-item').hide().addClass('fade-out-filter');
 
-      $('.filter-btn').removeClass('filter-selected selected').addClass('underlined');
-      $(this).removeClass('underlined').addClass('filter-selected selected');
+        $('.filter-btn').removeClass('filter-selected selected').addClass('underlined');
+        $(this).removeClass('underlined').addClass('filter-selected selected');
 
-      filter = $(this).attr('gallery-filter');
-      $('.masonry-item').each( function() {
-          if (filter == 'all') {
-              $(this).show().removeClass('fade-out-filter').addClass('fade-in-filter');
-          } else {
-              $(this).hide().removeClass('fade-out-filter').addClass('fade-out-filter');
-              if ($(this).attr('gallery-category') == filter) {
+        filter = $(this).attr('gallery-filter');
+        $('.masonry-item').each(function () {
+            if (filter == 'all') {
                 $(this).show().removeClass('fade-out-filter').addClass('fade-in-filter');
-              }
-          }
-      });
-  });
+            } else {
+                $(this).hide().removeClass('fade-out-filter').addClass('fade-out-filter');
+                if ($(this).attr('gallery-category') == filter) {
+                    $(this).show().removeClass('fade-out-filter').addClass('fade-in-filter');
+                }
+            }
+        });
+    });
 });
 
 
-[].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
-  img.setAttribute('src', img.getAttribute('data-src'));
-  img.onload = function() {
-    img.removeAttribute('data-src');
-  };
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function () {
+        img.removeAttribute('data-src');
+    };
 });
