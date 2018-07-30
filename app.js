2d971bb357fc30d981fe52bfbaa0c80f27daf7b4
Troy Curnel <NearSingularity@troycurnel.com>
2018-07-30 13:13:50 -0700

    Change port

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ var host = process.env.HOST
 // Add Favicon
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
