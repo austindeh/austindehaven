568dbd2a79926c95105c16b08c2f4242c9be611e
Troy Curnel <NearSingularity@troycurnel.com>
2018-07-30 13:19:06 -0700

    yeah change bounds

--- public/javascript/project-page.js
+++ public/javascript/project-page.js
@@ -1,8 +1,8 @@
 // Project Navbar
 var lastScrollTop = 1;
 
-window.addEventListener("scroll", function(){  
-   var st = window.pageYOffset || document.documentElement.scrollTop + '10px';  
+window.addEventListener("scroll", function(){
+   var st = window.pageYOffset || document.documentElement.scrollTop + '10px';
    if (st > lastScrollTop){
        document.getElementById("project-navigation").style.top = "-80px";
    } else {
@@ -18,7 +18,7 @@ $('.carousel').each(function(){
 
 Draggable.create('.carousel-items', {
     type: 'x',
-    bounds: '.carousel',
+    bounds: '.carousel-items',
     // throwProps: true,
     edgeResistance: 0.75,
     dragResistance: -2,
